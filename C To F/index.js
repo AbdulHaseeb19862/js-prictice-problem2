429222b96ee8c1daadd0e43107aa1872b6840c28
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-07-08 13:09:05 +0500

    problems of js

---
+++ C To F/index.js
@@ -0,0 +1,25 @@
+//Celsius To Fahrenheit
+
+// var celsius = +prompt("Enter a value");
+// var fahrenheit = (9 / 5) * celsius + 32;
+// console.log(`F = ${fahrenheit}`);
+
+//Fahrenheit To Celsius
+
+// var fahrenheit = +prompt("Enter a value");
+// var celsius = ((fahrenheit - 32) * 5) / 9;
+// console.log(`Celsius : ${celsius}`);
+
+//FIND THE SMALLEST NUMBER IN AN ARRAY
+
+// var array = [100, 23, 1, 63, 78];
+// var smallest = array[0];
+// for (var i = 0; i < array.length; i++) {
+//   if (smallest > array[i]) {
+//     smallest = array[i];
+//   }
+// }
+// console.log(`The smallest numbers ${smallest}`);
+
+//REVERSE AN ARRAY
+var array = [100, 23, 1, 63, 78];
